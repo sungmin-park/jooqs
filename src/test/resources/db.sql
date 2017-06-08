2e835f5b68a5a799339a97eba23d80335f296918
Sung Min Park <vamf12@Sungs-MacBook-Pro.local>
2017-06-08 23:05:04 +0900

    기본 jooq 생성기 추가

---
+++ src/test/resources/db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE "user" (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  name varchar(255) default NULL
+);
+
+INSERT INTO "user" (name) VALUES ('Maia'),('Cally'),('Ila'),('Chaney'),('Zeus'),('Brody'),('Nolan'),('Genevieve'),('Charlotte'),('Julian');
+INSERT INTO "user" (name) VALUES ('Chanda'),('Ryder'),('Aristotle'),('Garrison'),('Walker'),('Hakeem'),('Meredith'),('Carissa'),('Marcia'),('Orson');
+INSERT INTO "user" (name) VALUES ('Nero'),('Geoffrey'),('Keely'),('Zeus'),('Uriel'),('Harriet'),('Skyler'),('Cairo'),('Deirdre'),('Colby');
+INSERT INTO "user" (name) VALUES ('Perry'),('Rhonda'),('Naomi'),('Dylan'),('Sylvia'),('Randall'),('Lillith'),('Jocelyn'),('Jerry'),('Ruth');
+INSERT INTO "user" (name) VALUES ('Victor'),('Orla'),('TaShya'),('Fredericka'),('Kelly'),('Byron'),('Beverly'),('Duncan'),('Joel'),('Nerea');
+INSERT INTO "user" (name) VALUES ('Azalia'),('Pamela'),('Virginia'),('Pearl'),('Nell'),('India'),('Tashya'),('Reece'),('Leslie'),('Mari');
+INSERT INTO "user" (name) VALUES ('Hiroko'),('Omar'),('Kelly'),('Jocelyn'),('Dominic'),('Jayme'),('Raya'),('Ori'),('Priscilla'),('Shellie');
+INSERT INTO "user" (name) VALUES ('Galena'),('Kevin'),('Aurelia'),('Charde'),('Roth'),('Ruth'),('Amela'),('Aphrodite'),('Lois'),('Madeson');
+INSERT INTO "user" (name) VALUES ('Georgia'),('Peter'),('Melvin'),('Plato'),('Griffin'),('Timothy'),('Michael'),('Kai'),('Chaim'),('Wyoming');
+INSERT INTO "user" (name) VALUES ('Ray'),('Gabriel'),('Natalie'),('Tarik'),('Odysseus'),('Lydia'),('Jeremy'),('Destiny'),('McKenzie'),('Ronan');
+
+INSERT INTO "user" (name) VALUES ('Galena'),('Kevin'),('Aurelia'),('Charde'),('Roth'),('Ruth'),('Amela'),('Aphrodite'),('Lois'),('Madeson');
+INSERT INTO "user" (name) VALUES ('Georgia'),('Peter'),('Melvin'),('Plato'),('Griffin'),('Timothy'),('Michael'),('Kai'),('Chaim'),('Wyoming');
+INSERT INTO "user" (name) VALUES ('Ray'),('Gabriel'),('Natalie');
